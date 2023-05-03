b86fa8aec6e8904a58372c5e5d5d624543798154
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-05-03 17:17:30 +0900

    made middleware and new router for searching
    by using regular expression

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,26 +1,29 @@
 import videoModel from "../models/Video.js";
 
+  // home
   export const home = async(req, res) => {
-    const videos = await videoModel.find({}) 
+    const videos = await videoModel.find({}).sort({}) 
     return res.render('home', { pageTitle: 'Home', videos})
   };
 
+  // watch video
   export const watchVideo = async (req, res) => {
     const {id} = req.params;
     const video = await videoModel.findById(id)
     if(!video) {
-      return res.render('404', {pageTitle: 'video not found',})
+      return res.render('404_Error', {pageTitle: 'video not found',})
     }
-    return  res.render('watch', {pageTitle: `${video.title}`, video}); 
+    return  res.render('./Video/watchVideo', {pageTitle: `${video.title}`, video}); 
   };
 
+  // edit video
   export const getEditVideo = async (req, res) => {
     const {id} = req.params;
     const video = await videoModel.findById(id)
     if(!video) {
-      return res.render('404', {pageTitle: 'video not found',})
+      return res.render('404_Error', {pageTitle: 'video not found',})
     }
-    return res.render('edit', {pageTitle: `Edit ${video.title}`, video});
+    return res.render('./Video/editVideo', {pageTitle: `Edit ${video.title}`, video});
   };
 
   export const postEditVideo = async (req,res) => {
@@ -28,9 +31,8 @@ import videoModel from "../models/Video.js";
     const {title, description, hashtags} = req.body
     const video = await videoModel.exist({_id: id})
     if(!video) {
-      return res.render('404', {pageTitle: 'video not found',})
+      return res.render('404_Error', {pageTitle: 'video not found',})
     }
-    // update video
     await videoModel.findByIdAndUpdate(id, {
       title,
       description,
@@ -39,8 +41,9 @@ import videoModel from "../models/Video.js";
     return res.redirect(`/videos/${id}`) 
   }
   
+  // upload video
   export const getUploadVideo = (req, res) => {
-    res.render('upload', {pageTitle: `Upload Video`})
+    res.render('./Video/uploadVideo', {pageTitle: `Upload Video`})
   }
 
   export const postUploadVideo = async (req, res) => {
@@ -54,13 +57,31 @@ import videoModel from "../models/Video.js";
       return res.redirect('/')
 
     } catch (error) {
-      return res.render('upload', {
+      return res.render('./Video/uploadVideo', {
         pageTitle: `Upload Video`,
         errorMessage: error._message
       })
     }
   }
 
-  export const deleteVideo = (req, res) => {
-    res.send('Helloasdasdasdsadsadssssd!');
+// Delete a Video
+  export const getDeleteVideo = async (req, res) => {
+    const {id} = req.params
+    await videoModel.findByIdAndDelete({_id: id})
+    return res.redirect('/')
   };
+
+// Search Videos
+  export const searchVideo = async (req, res) => {
+    const {keyword} = req.query
+    let videos = []
+    if(keyword) {
+      videos = await videoModel.find({
+        title: {
+          // search videos by using regular expression, i => no matter what the lowercase or the upper case
+          $regex: new RegExp(`${keyword}`, 'i')
+        }
+      })
+    }
+    return res.render('./Video/searchVideo', {pageTitle: "Search", videos})
+  }
--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {watchVideo, getEditVideo,postEditVideo, deleteVideo, getUploadVideo, postUploadVideo } from '../controller/videoController.js';
+import {watchVideo, getEditVideo,postEditVideo, getDeleteVideo, getUploadVideo, postUploadVideo, searchVideo } from '../controller/videoController.js';
 
 export const videoRouter = express.Router();
 
@@ -11,6 +11,10 @@ export const videoRouter = express.Router();
 // 앞에 :id - id를 붙여준 이유는 해당 req.params.id 와 같이 key 값(id)을 주기 위해
 videoRouter.get('/:id([0-9a-f]{24})', watchVideo);
 videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEditVideo).post(postEditVideo)
+videoRouter.route('/:id([0-9a-f]{24})/delete').get(getDeleteVideo)
 videoRouter.route('/upload').get(getUploadVideo).post(postUploadVideo)
+videoRouter.get('/search', searchVideo);
+
+
 
 export default videoRouter;
